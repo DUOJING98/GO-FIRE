1bd1c0662dfe5eb3d63f26c633028461313ba360
DUOJING98 <duojing98@gmail.com>
2025-07-03 10:09:12 +0900

    αCountDownManager

--- Assets/Script/GameManager/CountDownManager.cs
+++ Assets/Script/GameManager/CountDownManager.cs
@@ -8,9 +8,11 @@ public class CountDownManager : MonoBehaviour
     public float minDelay = 1f;
     public float maxDelay = 3f;
 
+    public Text signalText;
+
     public UnityEvent onReadyStart;
     public UnityEvent onGoSignal;
-    public UnityEvent<string> onFakeSignal;
+    public UnityEvent onFakeSignal;
 
 
     private void Start()
@@ -21,11 +23,37 @@ public class CountDownManager : MonoBehaviour
     IEnumerator CountdownRoutine()
     {
         yield return ShowNumber("3");
+        yield return ShowNumber("2");
+        yield return ShowNumber("1");
+        signalText.text = "èÄîı...";
+        onReadyStart.Invoke();
+        
+        yield return new WaitForSeconds(Random.Range(minDelay, maxDelay));
+
+        int rand = Random.Range(0, 2);//0:fake,1:Go!
+        if(rand == 0)
+        {
+            string[] fakeSignals = { "WAIT!", "DOG!", "START!", "READY!", "HOLD!" };
+            string fake = fakeSignals[Random.Range(0, fakeSignals.Length)];
+            signalText.text = fake;
+            onFakeSignal.Invoke();
+        }
+        else
+        {
+            signalText.text = "GO!";
+            onGoSignal.Invoke();
+        }
     }
 
 
     IEnumerator ShowNumber(string number)
     {
+        signalText.text = number;
         yield return new WaitForSeconds(1f);
+    }
+
+    public void ClearText()
+    {
+        signalText.text = " ";
     }
 }
