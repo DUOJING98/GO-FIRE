ac5c0e59a03b3743b291d83c69dd5eeb5b47842d
DUOJING98 <duojing98@gmail.com>
2025-07-03 09:58:21 +0900

    (new)CountDownManager

--- Assets/Script/GameManager/CountDownManager.cs
+++ Assets/Script/GameManager/CountDownManager.cs
@@ -1,21 +1,31 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class CountDownManager : MonoBehaviour
 {
-    public float countdownTime = 3f;
-    public Text countdownText;
-    public static bool canAct=false;
+    public float minDelay = 1f;
+    public float maxDelay = 3f;
+
+    public UnityEvent onReadyStart;
+    public UnityEvent onGoSignal;
+    public UnityEvent<string> onFakeSignal;
 
 
-    //private void Start()
-    //{
-    //    canAct = false;
-    //    StartCoroutine(nameof(CountdownRoutine);
-    //}
+    private void Start()
+    {
+        StartCoroutine(nameof(CountdownRoutine));
+    }
 
-    //private System.Collections.IEnumerator CountdownRoutine()
-    //{
+    IEnumerator CountdownRoutine()
+    {
+        yield return ShowNumber("3");
+    }
 
-    //}
+
+    IEnumerator ShowNumber(string number)
+    {
+        yield return new WaitForSeconds(1f);
+    }
 }
