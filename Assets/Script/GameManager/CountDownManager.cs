698a64a83a1fc67a1b4f2a8866b1d08de4815a82
DUOJING98 <duojing98@gmail.com>
2025-07-03 10:10:21 +0900

    Squashed commit of the following:
    commit 1bd1c0662dfe5eb3d63f26c633028461313ba360
    Author: DUOJING98 <duojing98@gmail.com>
    Date:   Thu Jul 3 10:09:12 2025 +0900
    
        αCountDownManager
    
    commit ac5c0e59a03b3743b291d83c69dd5eeb5b47842d
    Author: DUOJING98 <duojing98@gmail.com>
    Date:   Thu Jul 3 09:58:21 2025 +0900
    
        (new)CountDownManager

--- Assets/Script/GameManager/CountDownManager.cs
+++ Assets/Script/GameManager/CountDownManager.cs
@@ -1,21 +1,59 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class CountDownManager : MonoBehaviour
 {
-    public float countdownTime = 3f;
-    public Text countdownText;
-    public static bool canAct=false;
+    public float minDelay = 1f;
+    public float maxDelay = 3f;
+
+    public Text signalText;
+
+    public UnityEvent onReadyStart;
+    public UnityEvent onGoSignal;
+    public UnityEvent onFakeSignal;
 
 
-    //private void Start()
-    //{
-    //    canAct = false;
-    //    StartCoroutine(nameof(CountdownRoutine);
-    //}
+    private void Start()
+    {
+        StartCoroutine(nameof(CountdownRoutine));
+    }
 
-    //private System.Collections.IEnumerator CountdownRoutine()
-    //{
+    IEnumerator CountdownRoutine()
+    {
+        yield return ShowNumber("3");
+        yield return ShowNumber("2");
+        yield return ShowNumber("1");
+        signalText.text = "èÄîı...";
+        onReadyStart.Invoke();
+        
+        yield return new WaitForSeconds(Random.Range(minDelay, maxDelay));
 
-    //}
+        int rand = Random.Range(0, 2);//0:fake,1:Go!
+        if(rand == 0)
+        {
+            string[] fakeSignals = { "WAIT!", "DOG!", "START!", "READY!", "HOLD!" };
+            string fake = fakeSignals[Random.Range(0, fakeSignals.Length)];
+            signalText.text = fake;
+            onFakeSignal.Invoke();
+        }
+        else
+        {
+            signalText.text = "GO!";
+            onGoSignal.Invoke();
+        }
+    }
+
+
+    IEnumerator ShowNumber(string number)
+    {
+        signalText.text = number;
+        yield return new WaitForSeconds(1f);
+    }
+
+    public void ClearText()
+    {
+        signalText.text = " ";
+    }
 }
