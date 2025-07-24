78890d6d5fad5a49c35f45083eb1b16f225cfc61
Tian <hongji98313@gmail.com>
2025-07-24 12:24:01 +0900

    123

--- Assets/Script/Iteam/Kusa.cs
+++ Assets/Script/Iteam/Kusa.cs
@@ -5,23 +5,43 @@ public class Kusa : MonoBehaviour
 {
     public float moveSpeed = 2f;
     public float rotateSpeed = 180f;
-    public float endX = 10f;
+    public float endX = 12f;
+
+    private int direction = 1;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (transform.position.x > 0)
+        {
+            direction = -1;
+            rotateSpeed = Mathf.Abs(rotateSpeed);
+            FlipSprite();
+        }
+        else
+        {
+            direction = -1;
+            rotateSpeed = -Mathf.Abs(rotateSpeed);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right * moveSpeed * Time.deltaTime, Space.World);
+        transform.Translate(Vector3.right * direction * moveSpeed * Time.deltaTime, Space.World);
 
-        transform.Rotate(Vector3.forward, -rotateSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.forward, rotateSpeed * Time.deltaTime);
 
-        if (transform.position.x > endX)
+        if ((direction == 1 && transform.position.x > endX)||
+            (direction == -1 && transform.position.x < -endX)) 
         {
             Destroy(gameObject);
         }
+    }
+
+    void FlipSprite()
+    {
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 }
