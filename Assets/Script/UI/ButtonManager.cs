1bbf662f8a52a70c58115d86a47b4562d4df4e9d
24ci0203sungmin <24ci0203@jec.ac.jp>
2025-10-01 16:39:02 +0900

    zhang

---
+++ Assets/Script/UI/ButtonManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonManager : MonoBehaviour
+{
+    public GameObject firstSelected;
+
+
+
+    void Start()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+
+        EventSystem.current.SetSelectedGameObject(firstSelected);
+
+
+    }
+
+}
